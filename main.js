ad920fa0126db94f91920faebcca56f3a8904011
Grant Paschal <gpass94@gmail.com>
2017-06-15 06:44:08 -0400

    Modified Commit

--- main.js
+++ main.js
@@ -1,19 +1,56 @@
 // 1. Fetch your users data
 // 2. Loop over the data
 // 3. Display it in the `.customers` element
+  let customers = document.querySelector(".customers");
+
+(function () {
+
+  'use strict';
+
 fetch("https://randomuser.me/api/?results=15")
   .then(function (response){
 
     response.json().then(function (data) {
-      for (var i = 0; i < data.length; i++) {
-        let customers = data.results;
-
-      }
       console.log(data);
+      console.log(data.results[0]);
+
+      let users = data.results;
+      let userData = "";
+
+      for (var i = 0; i < users.length; i++) {
+        userData += "<img src=" + users[i].picture.large + ">";
+        userData += users[i].name.first + " " + users[i].name.last;
+        userData += " " + users[i].email;
+        userData += " " + users[i].location.street + " " + users[i].location.city + " " + users[i].location.state;
+        userData += " " + users[i].cell;
+        userData += "<br>";
+      }
+
+
+      customers.innerHTML = userData;
+
+
+
+      //}
+
     })
 
-  });
+  })
+})();
 
 
-  // Your Code Goes Here
-'use strict';
+  //large: (url-string)
+  //name(first: string)
+  //last:
+  //}
+  //email:
+
+
+//location:
+//street
+//city:
+//state:
+
+
+
+// cell:
